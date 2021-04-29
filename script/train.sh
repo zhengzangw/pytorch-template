b26bf899eedaf1a4ff14e3aeda941953351d512b
zhengzangw <zhengzangw@163.com>
2021-04-29 22:25:44 +0800

    reorganize

--- script/train.sh
+++ script/train.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-#SBATCH --time=02:00:00
+#SBATCH --time=04:00:00
 #SBATCH --qos=regular
 #SBATCH --constraint=gpu
 #SBATCH --nodes=1
-#SBATCH --gpus=1
+#SBATCH --gpus=4
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=12
 
 ##SBATCH --mail-type=ALL
 #SBATCH --output=%j-%x.%u.out
 #SBATCH --job-name=default
 #SBATCH --account=m3691
 
-conda activate /global/homes/z/zangwei/.conda/envs/zw2
+#SBATCH --signal=SIGUSR1@90
 
-srun python src/train.py --config $@
+export NCCL_DEBUG=INFO
+export PYTHONFAULTHANDLER=1
+
+conda activate /global/homes/z/zangwei/.conda/envs/pcss
+
+srun python -m project.train $@
